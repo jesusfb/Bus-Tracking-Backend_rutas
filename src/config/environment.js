b6eecec216f8643c28bcc4015bb295e88f25c651
jesusfb <jfigueroa.beltran@gmail.com>
2024-05-08 14:59:38 -0700

    Update environment.js

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
     NODE_ENV : process.env.NODE_ENV || "development",
     PORT : process.env.PORT || 3000,
-    MONGO_URI : process.env.MONGO_URI || "mongodb+srv://jesusfb:<password>@cluster0.yx9sjqo.mongodb.net/bustj",
+    MONGO_URI : process.env.MONGO_URI || "mongodb+srv://jesusfb:Dove3229-@cluster0.yx9sjqo.mongodb.net/bustj",
     JWT_SECRET : process.env.JWT_SECRET || "JWT_SECRET",
 }
